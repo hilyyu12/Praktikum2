aa49c47fc9e153041c053741832e5a16f05af159
abiyyu hilmi <abiyyumi1112@gmail.com>
2019-01-07 14:46:25 +0700

    selesai

---
+++ latihan7.php
@@ -0,0 +1,16 @@
+<?php
+$arrbuah = array ("mangga", "apel", "pisang", "jeruk",);
+echo $arrbuah[0]; //mangga
+echo '<br>';
+echo $arrbuah[3]; //jeruk
+echo '<br>';
+
+$arrwarna = array();
+$arrwarna[] = "merah";
+$arrwarna[] = "biru";
+$arrwarna[] = "hijau";
+$arrwarna[] = "putih";
+echo $arrwarna[0];  //merah
+echo '<br>';
+echo $arrwarna[2]; //hijau
+?>
---
+++ latihan8.php
@@ -0,0 +1,15 @@
+<?php
+$arrnilai = array ('ani' => 80, 'otim' => 90, 'ana' => 75, 'budi' => 85);
+echo $arrnilai['ani']; //80
+echo "<br>";
+echo $arrnilai['otim']; //90
+echo "<br>";
+
+$arrnilai = array();
+$arrnilai['ami'] = 80;
+$arrnilai['asma'] = 95;
+$arrnilai['sri'] = 77;
+echo $arrnilai['asma']; //95
+echo "<br>";
+echo $arrnilai['ami']; //80
+?>
